e9cde13e713df1d36a01f83fb1d54245f5602e3a
apricot <just.anastasia.terenteva@gmail.com>
2024-02-16 13:03:01 +1100

    remove keys

--- client/src/firebaseConfig.js
+++ client/src/firebaseConfig.js
@@ -1,19 +1,50 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+
+import dotenv from 'dotenv';
+dotenv.config({ path: '../../.env' });
+
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCsNBjdOSiiWJCwZH-MvWMcVsJEX9HAmZs",
-    authDomain: "server-practice-a0dd1.firebaseapp.com",
-    projectId: "server-practice-a0dd1",
-    storageBucket: "server-practice-a0dd1.appspot.com",
-    messagingSenderId: "221311199817",
-    appId: "1:221311199817:web:255e5efa1d4746e671ad98"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const auth = getAuth();
 
+// Sign up new users
+createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        // ...
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+    });
+
+// Sign in existing users
+signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        // ...
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+    });
 export default app;
